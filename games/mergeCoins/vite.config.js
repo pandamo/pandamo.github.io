7c7c4fa1ae10717849cf818525e601c571d245a7
moyx <moyx@aisidi.com>
2026-03-18 16:29:24 +0800

    no message

--- games/mergeCoins/vite.config.js
+++ games/mergeCoins/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/games/mergeCoins/',
+  base: './',
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
